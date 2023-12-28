8fa7ddcbc5ff2bcdd7a8f98f21ec05f40584a398
Ian Skelskey <ianskelskey@gmail.com>
2023-12-28 00:23:03 -0500

    Set background for top and bot navs

--- src/main/java/io/ianskelskey/wizard/controller/WizardController.java
+++ src/main/java/io/ianskelskey/wizard/controller/WizardController.java
@@ -1,14 +1,24 @@
 package io.ianskelskey.wizard.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 public class WizardController {
+
+    private String primaryColor = "#3379b7";
 
     public HBox topNavigation;
     public HBox bottomNavigation;
 
-    public WizardController() {
-
+    @FXML
+    protected void initialize() {
+        Color backgroundColor = Color.web(primaryColor);
+        Background background = new Background(new BackgroundFill(backgroundColor, null, null));
+        topNavigation.setBackground(background);
+        bottomNavigation.setBackground(background);
     }
 
 
