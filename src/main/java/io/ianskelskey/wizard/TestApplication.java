76ecbf0ad7f00f017bbeec76442adc04c87d50dc
Ian Skelskey <ianskelskey@gmail.com>
2023-12-28 23:38:34 -0500

    Update

--- src/main/java/io/ianskelskey/wizard/TestApplication.java
+++ src/main/java/io/ianskelskey/wizard/TestApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TestApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TestApplication.class.getResource("wizard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TestApplication.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Wizard!");
         stage.setScene(scene);
--- src/main/java/io/ianskelskey/wizard/controller/WizardController.java
+++ src/main/java/io/ianskelskey/wizard/controller/WizardController.java
@@ -1,5 +1,7 @@
 package io.ianskelskey.wizard.controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -8,18 +10,29 @@ import javafx.scene.paint.Color;
 
 public class WizardController {
 
-    private String primaryColor = "#3379b7";
+    private final StringProperty primaryColor = new SimpleStringProperty("#3379b7");
 
     public HBox topNavigation;
     public HBox bottomNavigation;
 
     @FXML
     protected void initialize() {
-        Color backgroundColor = Color.web(primaryColor);
+        styleComponents();
+    }
+
+    private void styleComponents() {
+        Color backgroundColor = Color.web(getPrimaryColor());
         Background background = new Background(new BackgroundFill(backgroundColor, null, null));
         topNavigation.setBackground(background);
         bottomNavigation.setBackground(background);
     }
 
+    public void setPrimaryColor(String color) {
+        primaryColor.set(color);
+    }
+
+    public String getPrimaryColor() {
+        return primaryColor.get();
+    }
 
 }
